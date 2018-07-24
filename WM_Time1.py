e748a22a849978eeeee004ad9b5c7deac4fd7eed
Slimak1488 <strapon1488@gmail.com>
2018-07-24 12:17:24 +0300

    initial project

---
+++ WM_Time1.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory,jsonify
+from werkzeug.utils import secure_filename
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def clock():
+    return render_template('clock.html')
+
+
+@app.route("/time", methods=['GET'])
+def get_time():
+    return jsonify(datetime.datetime.now().strftime('%H'), datetime.datetime.now().strftime('%M'))
+
+'''''''''
+@app.route("/min", methods=['GET'])
+def get_min():
+    return jsonify(datetime.datetime.now().strftime('%M'))
+'''''''''
+
+app.run()
